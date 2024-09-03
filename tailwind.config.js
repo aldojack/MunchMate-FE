8850fc0a3fabfe0b1c86c7c7a86c16c6a00146e0
Alan Jack <aldojack@hotmail.co.uk>
2024-09-03 07:49:01 +0100

    Installed material base and added hero image to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('./src/assets/images/timelapsed-burger.jpg')"
+      }
+    },
   },
   plugins: [],
 }
