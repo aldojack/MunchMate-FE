933c43bd691fc95865d1075882455dced4d29606
Alan Jack <aldojack@hotmail.co.uk>
2024-11-30 13:32:22 +0000

    Implement filters for recipe page to include filter by source, include/exclude ingredients and cooking time

--- src/pages/Recipes/Recipes.tsx
+++ src/pages/Recipes/Recipes.tsx
@@ -2,16 +2,37 @@ import { useEffect, useState } from "react";
 import { RecipeDTO } from "../../types";
 import RecipeCard from "../../components/Recipe/RecipeCard";
 import { getAllRecipes } from "../../services/recipeServices";
+import Select from "react-select";
+import { getAllIngredients } from "../../services/ingredientServices";
+
+interface Filters {
+  source: string;
+  includeIngredients: string[];
+  excludeIngredients: string[];
+  maxCookingTime: number | null;
+}
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState<RecipeDTO[]>();
-  const [filters, setFilters] = useState({
+  const [filters, setFilters] = useState<Filters>({
     source: "",
     includeIngredients: [],
     excludeIngredients: [],
     maxCookingTime: null,
   });
   const [sources, setSources] = useState<string[]>();
+  const [ingredients, setIngredients] = useState<string[]>();
+
+  interface SelectOptions {
+    label: string;
+    value: string | number;
+  }
+
+  const cookTimeOptions: SelectOptions[] = [
+    { label: "Under 30 Mins", value: 30 },
+    { label: "Under an hour", value: 59 },
+    { label: "Over an hour", value: 60 },
+  ];
 
   useEffect(() => {
     const updateSourceOptions = (data: RecipeDTO[]) => {
@@ -20,10 +41,16 @@ const Recipes = () => {
       setSources([...uniqueSource]);
     };
 
+    const updateIngredientOptions = async () => {
+      const data = await getAllIngredients();
+      setIngredients(data.map((ingredient) => ingredient.name));
+    };
+
     const fetchRecipes = async () => {
       const data: RecipeDTO[] = await getAllRecipes();
       setRecipes(data);
       updateSourceOptions(data);
+      updateIngredientOptions();
     };
 
     fetchRecipes();
@@ -33,33 +60,87 @@ const Recipes = () => {
     if (!recipes) return [];
 
     let filteredList = [...recipes];
-    if (filters.source && filters.source !== "all") {
+
+    if (filters.source && filters.source !== "All") {
       filteredList = filteredList.filter(
         (recipe) => recipe.source.name === filters.source
       );
     }
-    if(filters.maxCookingTime){
-        const maxTime = filters.maxCookingTime;
-        switch(filters.maxCookingTime){
-            case 30: {
-                filteredList = filteredList.filter((recipe) => recipe.cookTime + recipe.prepTime <= maxTime)
-                break;
-            }
-            case 60: {
-                filteredList = filteredList.filter((recipe) => recipe.cookTime + recipe.prepTime <= maxTime)
-                break;
-            }
-            case 100: {
-                filteredList = filteredList.filter((recipe) => recipe.cookTime + recipe.prepTime >= maxTime)
-                break;
-            }
-            default: {
-                break
-            }
-        }
+
+    if (filters.maxCookingTime) {
+      const maxTime = filters.maxCookingTime;
+
+      if (filters.maxCookingTime === 30 || filters.maxCookingTime === 59) {
+        filteredList = filteredList.filter(
+          (recipe) => recipe.cookTime + recipe.prepTime <= maxTime
+        );
+      } else if (filters.maxCookingTime >= 100) {
+        filteredList = filteredList.filter(
+          (recipe) => recipe.cookTime + recipe.prepTime >= maxTime
+        );
+      }
     }
-    console.log(filteredList.length)
+
+    if (filters.includeIngredients.length >= 1) {
+      //Strict match
+      filteredList = filteredList.filter((recipe) =>
+        filters.includeIngredients.every((includeIngredient) =>
+          recipe.ingredients.some(
+            (ingredient) => ingredient.name === includeIngredient
+          )
+        )
+      );
+      //Looser match
+      // filteredList = filteredList.filter((recipe) =>
+      //   recipe.ingredients.some((ingredient) =>
+      //     filters.includeIngredients.includes(ingredient.name)
+      //   )
+      // );
+    }
+
+    if (filters.excludeIngredients.length >= 1) {
+      filteredList = filteredList.filter((recipe) =>
+        !recipe.ingredients.some((ingredient) =>
+          filters.excludeIngredients.includes(ingredient.name)
+        )
+      );
+    }
+    
     return filteredList;
+  };
+  const handleChange = (
+    selectedOption:
+      | { value: string | number; label: string }
+      | Array<{ value: string; label: string }>
+      | null,
+    actionMeta: { name?: string }
+  ) => {
+    if (actionMeta.name) {
+      if (Array.isArray(selectedOption)) {
+        // Multi-select: selectedOption is an array
+        const values = selectedOption.map((option) => option.value);
+        setFilters({ ...filters, [actionMeta.name]: values });
+      } else if (selectedOption) {
+        // Single-select: selectedOption is an object
+        setFilters({ ...filters, [actionMeta.name]: selectedOption.value });
+      } else {
+        // Handle clearing the selection
+        setFilters({ ...filters, [actionMeta.name]: null });
+      }
+    }
+  };
+
+  const populateSelectOptions = (list?: string[]) => {
+    if (!list) {
+      return [{ label: "loading", value: "loading" }];
+    }
+    const optionsList = list.map((selection: string) => ({
+      value: selection,
+      label: selection,
+    }));
+    optionsList.unshift({ label: "All", value: "All" });
+    // console.log(optionsList);
+    return optionsList;
   };
 
   const renderRecipes = (currentRecipes: RecipeDTO[]) => {
@@ -67,7 +148,7 @@ const Recipes = () => {
       return <RecipeCard key={`rc${recipe.id}`} recipe={recipe} />;
     });
     // Possibley handle no matches better later
-    return renderedList.length >0 ? renderedList : <p>No Recipes</p>
+    return renderedList.length > 0 ? renderedList : <p>No Recipes</p>;
   };
 
   return (
@@ -82,37 +163,52 @@ const Recipes = () => {
               className="border-2 border-blue-600 rounded-md"
               placeholder="Search by recipe"
             />
-            <div  className="flex space-x-2">
+            <div className="flex space-x-2">
               <label htmlFor="souce">
                 Source:
-                <select
+                <Select
+                  value={
+                    filters.source
+                      ? { value: filters.source, label: filters.source }
+                      : null
+                  }
                   name="source"
-                  className="border-2 border-black rounded-md"
-                  onChange={(e) =>
-                    setFilters({ ...filters, [e.target.name]: e.target.value })
-                  }
-                >
-                  <option value="all">All</option>
-                  {sources?.map((source) => (
-                    <option key={source} value={source}>
-                      {source}
-                    </option>
-                  ))}
-                </select>
+                  options={populateSelectOptions(sources)}
+                  onChange={(value, action) => handleChange(value, action)}
+                />
               </label>
-              <label htmlFor="maxCookingTime">Total Cooktime:
-                <select
+              <label htmlFor="maxCookingTime">
+                Total Cooktime:
+                <Select
                   name="maxCookingTime"
-                  className="border-2 border-black rounded-md"
-                  onChange={(e) =>
-                    setFilters({ ...filters, [e.target.name]: parseInt(e.target.value) })
-                  }
-                >
-                    <option value="0">All</option>
-                    <option value="30">Under 30 Mins</option>
-                    <option value="60">Under an hour</option>
-                    <option value="100">Over an hour</option>
-                </select>
+                  options={cookTimeOptions}
+                  onChange={(value, action) => handleChange(value, action)}
+                  isClearable
+                />
+              </label>
+              <label>
+                Includes:
+                <Select
+                  isMulti
+                  name="includeIngredients"
+                  options={populateSelectOptions(ingredients)}
+                  onChange={(value, action) => handleChange(value, action)}
+                />
+              </label>
+              <label>
+                Excludes:
+                <Select
+                  isMulti
+                  name="excludeIngredients"
+                  onChange={(value, action) => handleChange(value, action)}
+
+                  options={populateSelectOptions(
+                    ingredients?.filter(
+                      (ingredient) =>
+                        !filters.includeIngredients.includes(ingredient)
+                    )
+                  )}
+                />
               </label>
             </div>
           </div>
